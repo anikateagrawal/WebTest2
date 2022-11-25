fa651e3086aafce6b8d4772f878e423ec2603328
anikateagrawal <anikateag7316@gmail.com>
2022-11-25 22:39:51 +0530

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//theme script
 th = document.querySelectorAll('[name="theme"]');
 // console.log(th);
 th.forEach((theme) => {
@@ -14,3 +14,35 @@ function app() {
   });
 }
 document.onload = app();
+
+
+//api script
+document.getElementById('sh').addEventListener("click",display);
+
+function display(){
+    var v=document.getElementById('search').value;
+    req(v);
+}
+var data;
+function req(s){
+    const url="https://api.tvmaze.com/search/shows?q=";
+    const xhr=new XMLHttpRequest;
+    xhr.onload=function(){
+        data=this.response;
+        // data.forEach((d)=>{
+        //     console.log(data);
+        // })
+        console.log(data);
+        var c=document.getElementById('m');
+        var div=document.createElement('div');
+        c.appendChild(div);
+        div.className='mov';
+        
+    }
+    xhr.onerror=function(){
+        console.log('error');
+    }
+    xhr.open("GET",url+s);
+    xhr.send();
+    
+}
